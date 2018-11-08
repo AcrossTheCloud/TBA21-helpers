b425ebf42f2db716bbda305e9c81f90a4db49344
Matthew Berryman <matthew@acrossthecloud.net>
2018-11-08 17:04:28 +1100

    image tagging fixes

--- image_tagger.js
+++ image_tagger.js
@@ -19,7 +19,7 @@ exports.handler = async (event) => {
     };
 
     let rekognitionData = await rekognition.detectLabels(params).promise();
-    let requestData = {"key": srcKey, "labels": rekognitionData.Labels };
+    let requestData = {"key": event.srcKey, "labels": rekognitionData.Labels };
 
     let putParams = {
       TableName: process.env.IMAGE_TAG_TABLE,
