1dd2078a94a3df91c7fd3ebdbfca261ab028c594
matthew@acrossthecloud.net <matthew@acrossthecloud.net>
2019-01-30 14:01:30 +1100

    cross-region workaround for rekognition

---
+++ copy_for_rekognition.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event) => {
+
+  if (event.magic.match(/jpeg/) || event.magic.match(/png/)) {
+    const params = {
+      Bucket: process.env.REKOGNITION_BUCKET,
+      CopySource: `/${event.srcBucket}/${event.srcKey}`,
+      Key: event.srcKey
+    };
+    s3.copyObject(params, function (err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else callback(data);           // successful response
+    });
+  }
+}
