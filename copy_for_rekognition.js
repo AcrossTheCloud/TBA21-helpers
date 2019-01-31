2800a4fdce817a1bbc3b356c9625961663914550
matthew@acrossthecloud.net <matthew@acrossthecloud.net>
2019-02-01 09:50:08 +1100

    trace statements for copy to rekognition

--- copy_for_rekognition.js
+++ copy_for_rekognition.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
+  console.log(event.magic);
 
   if (event.magic.match(/jpeg/) || event.magic.match(/png/)) {
     const params = {
@@ -11,7 +12,10 @@ exports.handler = async (event) => {
     };
     s3.copyObject(params, function (err, data) {
       if (err) console.log(err, err.stack); // an error occurred
-      else callback(data);           // successful response
+      else {
+        console.log(data);
+        callback(data);   
+      }        // successful response
     });
   }
 }
