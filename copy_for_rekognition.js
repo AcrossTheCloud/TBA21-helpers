676df760280fb4b350b1fcfdb91dd2279a835713
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2019-04-01 16:54:35 +1100

    modify serverless and copy_for_rekognition

--- copy_for_rekognition.js
+++ copy_for_rekognition.js
@@ -5,7 +5,7 @@ exports.handler = (event, context, callback) => {
   console.log("doing copy_for_rekognition");
   console.log(event);
 
-  if (event.s3metadata.ContentType.toLowerCase().match(/(jpeg|png)/)) {
+  if (event.s3metadata.ContentType.toLowerCase().match(/(jpg|jpeg|png)/) || event.decodedSrcKey.toLowerCase().match(/\.hei[cf]$/)) {
     console.log('copying');
     const params = {
       Bucket: process.env.REKOGNITION_BUCKET,
@@ -13,10 +13,13 @@ exports.handler = (event, context, callback) => {
       Key: event.decodedSrcKey
     };
     s3.copyObject(params, function (err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
+      if (err) {
+        callback(err);
+        console.log(err, err.stack); // an error occurred
+      }
       else {
         console.log(data);
-        callback(data);   
+        callback(null, Object.assign(event,{rekognitionBucket: params.Bucket , rekognitionKey: params.Key }));   
       }        // successful response
     });
   }
