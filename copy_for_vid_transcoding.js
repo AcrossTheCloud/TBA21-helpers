3f9fc251b7281d6b0a9f2e6457f4c2ecc44ea935
Mehrdad Amirghasemi <mehrdad@acrossthecloud.net>
2019-05-06 14:19:04 +1000

    more logging

--- copy_for_vid_transcoding.js
+++ copy_for_vid_transcoding.js
@@ -2,12 +2,16 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 module.exports.handler = (event, context, callback) => {
+  
+   console.log('Doing copy_for_vid_transcoding ...');
 
     const params = {
      Bucket: process.env.TRANSCODE_BUCKET,
      CopySource: `/${event.srcBucket}/${event.decodedSrcKey}`,
      Key: event.decodedSrcKey
     };
+
+    console.log(params);
     s3.copyObject(params, function(err, data) {
       if (err) {
         console.log(err, err.stack);
