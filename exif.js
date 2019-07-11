d8b96ac40975446eb9b12d4f3391dfd8daaa2867
Matthew Berryman <matthew@acrossthecloud.net>
2019-07-11 12:15:33 +1000

    some logging if not

--- exif.js
+++ exif.js
@@ -7,6 +7,20 @@ const pgp = require('pg-promise')();
 const fs = require('fs');
 
 
+const path = require('path');
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'bin');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
 
 
 
