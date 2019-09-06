05932602b69a8cca04e3c92642f6f87e4a4a4583
Matthew Berryman <matthew@acrossthecloud.net>
2019-09-06 11:48:24 +1000

    convert wav > m4a

--- master.js
+++ master.js
@@ -18,10 +18,11 @@ module.exports.start = async (event, context, callback) => {
   let isImage= Boolean(data.ContentType.toLowerCase().match(/image/) || isHEI);
   let isJPEGPNG = Boolean(decodedSrcKey.toLowerCase().match(/(\.png|\.jpg|\.jpeg)$/));
   let isVideo = Boolean(data.ContentType.toLowerCase().match(/video/));
+  let isWav = Boolean(decodedSrcKey.match(/\.wav$/i));
 
   const params = {
     stateMachineArn: process.env.stateMachineArn,
-    input: JSON.stringify({srcBucket: srcBucket, srcKey: srcKey, decodedSrcKey: decodedSrcKey, s3metadata: data, isHEI,isImage,isJPEGPNG, isVideo}),
+    input: JSON.stringify({srcBucket: srcBucket, srcKey: srcKey, decodedSrcKey: decodedSrcKey, s3metadata: data, isHEI,isImage,isJPEGPNG, isVideo, isWav}),
     name: crypto.createHmac('sha256', srcKey + uuid()).digest('hex')
   }
 
