1fa15f29fcc97b5b4a782c0a5c950816616e5ebf
Matthew Berryman <matthew@acrossthecloud.net>
2019-09-13 11:10:16 +1000

    insert item_type on create_item

--- create_item.js
+++ create_item.js
@@ -10,7 +10,17 @@ const db = pgp(cn);
 
 const isValidUUID=(str) => (str.search(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)>=0)
 
-
+const downloadTextTypes = [
+  'msword',
+  'vnd.openxmlformats-officedocument.wordprocessingml.document', // docx
+  'vnd.ms-', // vnd.ms-powerpoint , excel etc
+  'vnd.openxmlformats', // pptx powerpoint
+  'vnd.oasis.opendocument', // OpenDocument
+  'epub+zip',
+  'rtf', // Rich text
+  'xml',
+  'vnd.amazon',
+];
 
 module.exports.handler = async (event, context, callback) => {
 
@@ -30,17 +40,44 @@ module.exports.handler = async (event, context, callback) => {
       cuuid=null;
     }
 
+    let type = null;
 
-
+    switch (true) {
+      case (/image/i.match(event.data.ContentType)):
+        type = 'Image';
+        break;
+      case (/audio/i.match(event.data.ContentType)):
+        type = 'Audio';
+        break;
+      case (/video/i.match(event.data.ContentType)):
+        type = 'Video';
+        break;
+      case (downloadTextTypes.reduce(
+        (overall, item) => {
+          return overall || (new RegExp(item), "i").match(event.data.ContentType)
+        },
+        false
+      )):
+        type = 'DownloadText'
+        break;
+      case (/text/i.match(event.data.ContentType)):
+        type = 'Text';
+        break;
+      case(/pdf/i.match(event.data.ContentType)):
+        type = 'PDF'
+        break;
+      default:
+        break;
+    }
 
     // Setup query
     let query = `INSERT INTO ${process.env.PG_ITEMS_TABLE}
-        (s3_key,status, contributor, created_at, updated_at)
-        VALUES ($1, $2, $3, current_timestamp, current_timestamp)
+        (s3_key,status, contributor, item_type created_at, updated_at)
+        VALUES ($1, $2, $3, $4, current_timestamp, current_timestamp)
         RETURNING s3_key;`;
 
     // Setup values
-    let values = [event.decodedSrcKey,false,cuuid];
+    let values = [event.decodedSrcKey,false,cuuid,type];
 
 
     // Execute
